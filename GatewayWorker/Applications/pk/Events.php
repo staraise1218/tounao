37892aaf4eabe2405fffb63e1eccd904d29a7d4a
shengli <363912809@qq.com>
2019-01-28 15:54:35 +0800

    event

--- GatewayWorker/Applications/pk/Events.php
+++ GatewayWorker/Applications/pk/Events.php
@@ -47,19 +47,19 @@ class Events
     * @param int $client_id 连接id
     * @param mixed $message 具体消息
     */
-   /*public static function onMessage($client_id, $message)
+   public static function onMessage($client_id, $message)
    {
         // 向所有人发送 
-        Gateway::sendToAll("$client_id said $message\r\n");
-   }*/
+        // Gateway::sendToAll("$client_id said $message\r\n");
+   }
    
    /**
     * 当用户断开连接时触发
     * @param int $client_id 连接id
     */
-   /*public static function onClose($client_id)
+   public static function onClose($client_id)
    {
        // 向所有人发送 
-       GateWay::sendToAll("$client_id logout\r\n");
-   }*/
+       // GateWay::sendToAll("$client_id logout\r\n");
+   }
 }
