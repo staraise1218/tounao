3af8ec1d085048b277aa6a7413ae861f3d700083
guofeng-mi <guofeng_mi@sina.com>
2019-01-31 17:20:57 +0800

    asdf

--- application/mobile/view/static/js/pk-control.js
+++ application/mobile/view/static/js/pk-control.js
@@ -97,7 +97,7 @@ ws.onmessage = function (event) {
 
         // if($user2_isright == 1) {
             console.log($(".choose-wrapper .choose-btn"))
-        $(".choose-wrapper .choose-btn").each(function(item) {
+        $($(".choose-wrapper .choose-btn")).each(function(item) {
             // if($(this).attr("data") == $user2_answer) {
                 console.log(2341241)
                 console.log($(this).attr("data"))
