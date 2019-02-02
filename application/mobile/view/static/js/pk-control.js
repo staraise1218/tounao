d874314d2fbab160e62926f51b226ac937a9d4e2
guofeng-mi <guofeng_mi@sina.com>
2019-02-02 23:56:57 +0800

    as

--- application/mobile/view/static/js/pk-control.js
+++ application/mobile/view/static/js/pk-control.js
@@ -257,7 +257,7 @@ $(document).ready(function(){
               `) 
               if(item.user_id == $user_id) {
                 // linear-gradient(#fddb92,#fee140)
-                $(".item").eq(index).css("background","linear-gradient(#3C8CE7,#00EAFF)")
+                $(".item").eq(index).css("background","linear-gradient(#92fe9d,#f9d423)")
                 $(".item").eq(index).find($(".pk")).css("display","none");
                 // var $paiming = index + 1 + '/' + $(".item").length;
                 // $("#paiming").text($paiming)
