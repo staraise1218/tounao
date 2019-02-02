407349762abadb0120d0a907f1fecee56a101b25
guofeng-mi <guofeng_mi@sina.com>
2019-02-02 12:52:27 +0800

    asf

--- application/mobile/view/static/js/pk-control.js
+++ application/mobile/view/static/js/pk-control.js
@@ -162,9 +162,10 @@ ws.onmessage = function (event) {
                 console.log("socket ajax 绑定成功")
                 $data = data;
                 $knowledgeList = data.data.knowledgeList
-                $touserinfo = data.data.userinfo.user_id
-                $to_user_id = data.data.touserinfo;
-                $userinfo = data.data.userinfo;
+                $to_user_id = data.data.userinfo.user_id
+
+                $userinfo = data.data.touserinfo;
+                $touserinfo = data.data.userinfo;
                 console.log("接受者 agreen*******************************************")
                 createUser();
             },
